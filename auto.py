3d819450456767db4a0a375761a1ccf0022ffe81
SCouto97 <samuelcoouto@hotmail.com>
2017-12-12 16:30:01 -0200

    Incrementado

--- auto.py
+++ auto.py
@@ -5,11 +5,11 @@
 #        institucional que utiliza DSpace.
 # @version: 0.2
 
-import bs4, requests, os, glob
+import bs4, requests, os, sys, glob, errno, mimetypes
 
 # função de parsing coleta atributos sobre os metadados e chama a função de baixar
 # as urls contidas nesses dados
-def parsing_module(my_file):
+def parsing_module(my_file, downl_dir):
     print("Parsing file %s...\n" % (my_file))
     local_file = open(my_file, 'rb')
     soup = bs4.BeautifulSoup(local_file, 'lxml')
@@ -18,47 +18,63 @@ def parsing_module(my_file):
     autoridade = desc_elems[1].text
     titulo = desc_elems[2].text
     data = desc_elems[3].text
+
+    ext_chunk = soup.find('span', {'class' : 'noprint'}).text
+    file_type = ext_chunk.split('/')
+    extension = file_type[1]
+    if extension.endswith(' ) '):
+        extension = extension[:-3]
+
+    print('\n\n\ndebug value: %s\n\n\n' % extension)
     url_elems = soup.findAll('a', {'class' : 'noprint', 'href' : True})
     refs = [] # cria uma lista de referências vazia
     for elem in url_elems:
         refs.append(elem['href'])
     valid_urls = [x for x in refs if "lex" not in x]
+    valid_urls = [x for x in valid_urls if "legislacao" not in x]
     for url in valid_urls:
-        download_module(url, titulo)
+        download_module(url, titulo, downl_dir)
 
 # função que realizará os downloads dos documentos dentro das páginas do Lexml
-def download_module(my_url, title):
+# problema no site http://legislacao.planalto.gov.br
+# TODO: tratar exceção 404
+def download_module(my_url, title, downl):
     local_title = title
     #para remover caracteres invalidos na hora de salvar o arquivo
     for ch in ['/', '-', ' ', ';', ':', '.', 'º', 'ª', ',']:
         local_title = local_title.replace(ch, '')
     print('Receiving from: %s' % my_url)
-    my_request = requests.get(my_url, timeout=5)
 
-    print('HTTP status code: %d' % my_request.status_code)
+    try:
+        my_request = requests.get(my_url, timeout=10)
+        print('HTTP status code: %d' % my_request.status_code)
+        print('Request OK\n')
+    except requests.exceptions.RequestException as e:
+        print('ERROR')
+        return
 
-    if(my_request.status_code == 200):
-        print('(OK)\n')
-    else:
-        print('(ERROR)\n')
-
-    with open('%s.pdf' % (local_title), 'wb') as f:
+    with open('%s/%s.pdf' % (downl, local_title), 'wb') as f:
         f.write(my_request.content)
 
-def sitemap_downloader(sitemap):
-    pass
-
 # função principal
-# TODO: Implementar módulo que utiliza wget. Quando salvar os arquivos, lembrar
-#       de salvar com a extensão .html no final.
+# TODO: Implementar função para adquirir os dados do sitemap lexml
 def main():
     #path = input('Escolha o diretorio que contem os arquivos a serem processados:\n')
-    path = './test2/'
-    if os.path.exists(path):
-        for filename in glob.glob(os.path.join(path, '*.html')):
+    file_path = './test2/'
+
+    directory = input("Select a directory to store the downloaded files:\n")
+    directory = "./%s" % (directory)
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    if os.path.exists(file_path):
+        for filename in glob.glob(os.path.join(file_path, '*.html')):
             local_file = open(filename, 'rb')
             soup = bs4.BeautifulSoup(local_file, 'lxml')
-            parsing_module(filename)
+            parsing_module(filename, directory)
             local_file.close()
     else:
         print("Forneca um caminho valido!\n")
