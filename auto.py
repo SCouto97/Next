24f564e4db8372bf92c0571ef81be40b16d7de35
SCouto97 <samuelcoouto@hotmail.com>
2017-12-14 13:25:34 -0200

    Tratamento de extensões adicionado/Função de download generalizada

--- auto.py
+++ auto.py
@@ -3,14 +3,10 @@
 # @email: samuelcoouto@hotmail.com
 # @desc: Programa que realiza submissões automáticas dentro de um repositório
 #        institucional que utiliza DSpace.
-# @version: 0.2
+# @version: 0.4
 
 import bs4, requests, os, sys, glob, errno, mimetypes, re
 
-def download(url, num_retries=2):
-    print('Downloading: %s' % url)
-    request = requests.get(url)
-    html = request.text
 # função de parsing coleta atributos sobre os metadados e chama a função de baixar
 # as urls contidas nesses dados
 def parsing_module(my_file, downl_dir):
@@ -22,7 +18,16 @@ def parsing_module(my_file, downl_dir):
     autoridade = desc_elems[1].text
     titulo = desc_elems[2].text
     data = desc_elems[3].text
-    #ext_elems = soup.findAll('span', {'class' : 'noprint'})    
+    ext_elems = soup.findAll('span', {'class' : 'noprint'})
+    l = []
+    for e in ext_elems:
+        r = re.findall('doc', e.text)
+        if(len(r) != 0):
+            l.append(r)
+    if(len(l) != 0):
+        extension = 'doc'
+    else:
+        extension = 'pdf'
     url_elems = soup.findAll('a', {'class' : 'noprint', 'href' : True})
     refs = [] # cria uma lista de referências vazia
     for elem in url_elems:
@@ -30,14 +35,21 @@ def parsing_module(my_file, downl_dir):
     valid_urls = [x for x in refs if "lex" not in x]
     valid_urls = [x for x in valid_urls if "legislacao" not in x]
     for url in valid_urls:
-        download_module(my_url=url, title=titulo, downl_path=downl_dir, ext='pdf')
+        download_module(my_url=url, title=titulo, downl_path=downl_dir, ext=extension)
 
 # função que realizará os downloads dos documentos dentro das páginas do Lexml
+# foi generalizada para baixar links do sitemap
 # problema no site http://legislacao.planalto.gov.br
 # TODO: tratar exceção 404
 def download_module(my_url, title, downl_path, ext):
     local_title = title
     #para remover caracteres invalidos na hora de salvar o arquivo
+    directory = "./%s" % (downl_path)
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
     for ch in ['/', '-', ' ', ';', ':', '.', 'º', 'ª', ',']:
         local_title = local_title.replace(ch, '')
     print('Receiving from: %s' % my_url)
@@ -49,32 +61,47 @@ def download_module(my_url, title, downl_path, ext):
         print('ERROR')
         return
 
-    with open('%s/%s.%s' % (downl_path, local_title, ext), 'wb') as f:
+    with open('%s/%s.%s' % (directory, local_title, ext), 'wb') as f:
         f.write(my_request.content)
 
+# função que obtém os arquivos de metadados dos sites do lexml
 def crawl_sitemap():
     url = input('escreva o sitemap que deseja baixar:\n');
     sitemap = requests.get(url).text
     links = re.findall('<loc>(.*?)</loc>', sitemap)
     count = 0
     for link in links:
-        print('link: %s\n' % link)
         count += 1
-    print('No de paginas acessadas: %d\n' % count)
+        sep = link.split('/')
+        nome = sep[4]
+        extension = 'html'
+        download_module(my_url=link, downl_path='sitemap_data', title=nome, ext=extension)
+    print('Number of pages crawled: %d\n' % count)
+
+# função criada para tentar corrigir alguns erros de extensão
+# feita para ser usada em loop dentro de um diretório
+def extension_converter_aux(filename, new_extension):
+    l = filename.split('.')
+    ext = l[len(l)-1]
+    l[len(l)-1] = new_extension
+    filename = '.'.join(l)
+    return filename
+# função para mudar extensões dentro de um diretório(usar quando tiver erros de extensão)
+def extension_converter(path):
+    old = '*.pdf'
+    new = 'html'
+    for filename in glob.glob(os.path.join(path, old)):
+        filename_new = extension_converter_aux(filename, new)
+        print('Old file name: %s' % filename_new)
+        os.rename(filename, filename_new)
+        print('Renamed to: %s' % new)
 
 # função principal
 # TODO: Implementar função para adquirir os dados do sitemap lexml
 def main():
 
-    file_path = './test2/'
+    file_path = './t3/'
     directory = input("Select a directory to store the downloaded files:\n")
-    directory = "./%s" % (directory)
-    try:
-        os.makedirs(directory)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-
     if os.path.exists(file_path):
         for filename in glob.glob(os.path.join(file_path, '*.html')):
             local_file = open(filename, 'rb')
@@ -83,5 +110,3 @@ def main():
             local_file.close()
     else:
         print("Forneca um caminho valido!\n")
-
-main()
