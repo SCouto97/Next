6612a07b0819412cfa18ebe21485c6c04b9d92d4
SCouto97 <samuelcoouto@hotmail.com>
2018-01-19 15:56:51 -0200

    Parsing STF parcial

--- parse_metadata.py
+++ parse_metadata.py
@@ -3,4 +3,11 @@ import bs4, re
 def parse_file(path_to_file):
     filename = open(path_to_file, 'rb')
     soup = bs4.BeautifulSoup(filename, 'lxml')
-    adv_pattern = re.compile('ADV.: ')
+    text = soup.text
+    adv = re.compile('ADV.: .*')
+    adv_list = adv.findall(text)
+    recte = re.compile('RECTE.: .*')
+    recte_list = recte.findall(text)
+    recdo = re.compile('RECDO.: .*')
+    recdo_list = re.findall(text)
+    dj = re.compile('Julgamento: \d/\d/\d') 
