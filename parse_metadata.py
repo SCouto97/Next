c1611ac3b1025111c3d57c5524c043481d140e0d
SCouto97 <samuelcoouto@hotmail.com>
2018-01-23 17:13:05 -0200

    Inclusão de um classificador para Coleções

--- parse_metadata.py
+++ parse_metadata.py
@@ -1,5 +1,11 @@
 import bs4, re
 
+class MetadataObject(object):
+    def __init__(self, recte_list=[], adv_list=[], recdo_list=[]):
+        self.recte_list = recte_list
+        self.adv_list = adv_list
+        self.recdo_list = recdo_list
+
 def parse_file(path_to_file):
     filename = open(path_to_file, 'rb')
     soup = bs4.BeautifulSoup(filename, 'lxml')
@@ -10,4 +16,41 @@ def parse_file(path_to_file):
     recte_list = recte.findall(text)
     recdo = re.compile('RECDO.: .*')
     recdo_list = re.findall(text)
-    dj = re.compile('Julgamento: \d/\d/\d') 
+
+
+    meta = MetadataObject(recte_list, adv_list, recdo_list)
+
+    return meta
+
+def find_category(name):
+    jurisp_keys = ['AgR', 'Acórdão', 'Súmula', 'Orientação Jurisprudencial', 'Relação'
+                   'RE', 'HC', 'AI', 'SS', 'CR', 'MI', 'RMS', 'MDI', 'Rcl', 'AO', 'AC',
+                   'ADI', 'Ext', 'MS']
+
+    leis_keys = ['Portaria', 'ATO', 'Ato', 'Constituição', 'Decreto', 'Lei', 'LEI',
+                 'Deliberação', 'Emenda', 'Enunciado', 'Instrução', 'INSTRUÇÃO',
+                 'Medida Provisória', 'Ordem de Serviço', 'Orientação Normativa',
+                 'PORTARIA', 'Provimento', 'Recomendação', 'Regimento Interno',
+                 'Resolução', 'RESOLUÇÃO']
+
+    prop_keys = ['PL', 'PROJETO DE LEI', 'PROJETO DE DECRETO', 'MPV', 'PLV', 'PRC'
+                 'PROPOSTA DE EMENDA', 'PEC']
+
+    other_keys = []
+
+    tipo = 'Outros'
+
+    for key in jurisp_keys:
+        if key in name:
+            tipo = 'Jurisprudências'
+            return tipo
+    for key in leis_keys:
+        if key in name:
+            tipo = 'Leis'
+            return tipo
+    for key in prop_keys:
+        if key in name:
+            tipo = 'Proposições Legislativas'
+            return tipo
+
+    return tipo
